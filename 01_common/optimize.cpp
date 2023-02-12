7d2ea7b01ecf03b57b9b50b41a840c8876a23bbf
sivcde0405 <gs22117@gs.hs.kr>
2023-02-13 02:28:29 +0900

    optimizer

---
+++ 01_common/optimize.cpp
@@ -0,0 +1,4 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx2,fma,sse2,ssse3,sse4,sse4.1,sse4.2,bmi,bmi2,popcnt,lzcnt")
+#pragma GCC target("sse,sse3") //only float
